06645fda073b635bfbe85c092692b77d0e43591e
Bijaya Poudel <poudelbijaya25@gmail.com>
2023-08-09 15:24:08 +0545

    Initial commit

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const expressLayout =require('express-ejs-layouts');
 const PORT=process.env.PORT || 3000;
 const path=require('path');
 
-app.get('/', (req,res)=> {
-    res.render('home');
-})
+
+//Assets
+app.use(express.static('public'));
+
+
 
 //Set Template Engine
 app.use(expressLayout)
@@ -15,6 +17,21 @@ app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
 
+
+app.get('/', (req,res)=> {
+    res.render('home');
+})
+
+app.get('/cart',(req,res)=> {
+    res.render('customers/cart')
+})
+
+app.get('/login',(req,res)=> {
+    res.render('auth/login');
+})
+app.get('/register',(req,res)=> {
+    res.render('auth/register');
+})
 
 
 app.listen(PORT,()=> {
